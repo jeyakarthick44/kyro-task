d24b3cf9ce57f3392dbb5b5c240ec62f2ec812da
jeyakarthick44 <jeyakarthick408@gmail.com>
2022-03-16 22:38:21 +0530

    data received from backend

--- front-end/src/Task.js
+++ front-end/src/Task.js
@@ -1,152 +1,66 @@
-import React from "react";
-// import axios from "axios";
-import { Box, Badge, Image, Heading } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, Badge, Heading } from "@chakra-ui/react";
 
-const Home = ({ propertiesForSale, propertiesForRent }) => {
-  //   const [data, setData] = useState([]);
-  //   console.log(propertiesForSale, propertiesForRent);
-  //   var options = {
-  //     method: "GET",
-  //     url: "https://bayut.p.rapidapi.com/properties/list",
-  //     params: {
-  //       locationExternalIDs: "5002,6020",
-  //       purpose: "for-rent",
-  //       hitsPerPage: "25",
-  //       page: "0",
-  //       lang: "en",
-  //       sort: "city-level-score",
-  //       rentFrequency: "monthly",
-  //       categoryExternalID: "4",
-  //     },
-  //     headers: {
-  //       "x-rapidapi-host": "bayut.p.rapidapi.com",
-  //       "x-rapidapi-key": "f14362c2f9msh256b7ee1078a2eap1c6fcejsn569f7774bf22",
-  //     },
-  //   };
+const Home = () => {
+  const [project, setProject] = useState("");
+  const fetchApi = () => {
+    axios
+      .get("http://localhost:5000")
+      .then(function (response) {
+        console.log(response.data.data);
+        setProject(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-  //   useEffect(() => {
-  //     <BallTriangle color="#00BFFF" height={80} width={80} />;
-  //     axios
-  //       .request(options)
-  //       .then(function (response) {
-  //         setData(response.data.hits);
-  //       })
-  //       .catch(function (error) {
-  //         console.error(error);
-  //       });
-  //   }, []);
+  useEffect(() => {
+    fetchApi();
+  }, []);
 
   return (
     <>
-      <Box
-        style={{ marginLeft: "20px", marginTop: "40px", marginBottom: "30px" }}
-        maxW="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        bg="whitelemon"
+      <Heading
+        as="h2"
+        size="xl"
+        isTruncated
+        style={{ textAlign: "center", marginTop: "20px" }}
       >
-        {/* <Image
-          src={item.coverPhoto.url}
-          style={{ height: "270px", width: "100%" }}
-          alt="home"
-        /> */}
-        <Box p="6">
-          <Box display="flex" alignItems="baseline">
-            <Badge borderRadius="full" px="2" colorScheme="teal">
-              whdoijho
-            </Badge>
-            <Box
-              color="gray.500"
-              fontWeight="semibold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-              ml="2"
-            >
-              whdoijho whdoijho baths
+        Rental House
+      </Heading>
+      {project.map((item) => (
+        <Box
+          style={{ marginLeft: "20px", marginTop: "40px" }}
+          maxW="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+        >
+          {/* <Image
+            src={item.coverPhoto.url}
+            style={{ height: "270px", width: "100%" }}
+            alt="home"
+          /> */}
+          <Box p="6">
+            <Box display="flex" alignItems="baseline">
+              <Badge borderRadius="full" px="2" colorScheme="teal">
+                {item.ProjectName}
+              </Badge>
+              <Box
+                color="gray.500"
+                fontWeight="semibold"
+                letterSpacing="wide"
+                fontSize="xs"
+                textTransform="uppercase"
+                ml="2"
+              >
+                {/* {item.ProjectName} */}
+                baths
+              </Box>
             </Box>
-          </Box>
-          <Box
-            mt="1"
-            fontWeight="semibold"
-            as="h4"
-            lineHeight="tight"
-            isTruncated
-          >
-            whdoijho
-          </Box>
-          <Box>
-            <Box as="span" color="gray.600" fontSize="sm">
-              whdoijho
-            </Box>
-          </Box>
-          <Box>
-            <Box
-              mt="1"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              whdoijho
-            </Box>
-            <span
-              mt="1"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            ></span>
-          </Box>
-        </Box>
-      </Box>
-      {/* ---------------------------------------------- */}
 
-      <Box
-        style={{ marginLeft: "20px", marginTop: "40px", marginBottom: "30px" }}
-        maxW="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        bg="black"
-      >
-        {/* <Image
-          src={item.coverPhoto.url}
-          style={{ height: "270px", width: "100%" }}
-          alt="home"
-        /> */}
-        <Box p="6">
-          <Box display="flex" alignItems="baseline">
-            <Badge borderRadius="full" px="2" colorScheme="teal">
-              whdoijho
-            </Badge>
-            <Box
-              color="gray.500"
-              fontWeight="semibold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-              ml="2"
-            >
-              whdoijho whdoijho baths
-            </Box>
-          </Box>
-          <Box
-            mt="1"
-            fontWeight="semibold"
-            as="h4"
-            lineHeight="tight"
-            isTruncated
-          >
-            whdoijho
-          </Box>
-          <Box>
-            <Box as="span" color="gray.600" fontSize="sm">
-              whdoijho
-            </Box>
-          </Box>
-          <Box>
             <Box
               mt="1"
               fontWeight="semibold"
@@ -154,18 +68,34 @@ const Home = ({ propertiesForSale, propertiesForRent }) => {
               lineHeight="tight"
               isTruncated
             >
-              whdoijho
+              {/* {item.ProjectName} */}
             </Box>
-            <span
-              mt="1"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            ></span>
+            <Box>
+              <Box as="span" color="gray.600" fontSize="sm">
+                {/* {item.price}/week */}
+              </Box>
+            </Box>
+            <Box>
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {/* {item.ProjectName} */}
+              </Box>
+              <span
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              ></span>
+            </Box>
           </Box>
         </Box>
-      </Box>
+      ))}
     </>
   );
 };
