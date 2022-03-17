89abd2af0d5d2c76fd3098f245a6537e763e7a36
jeyakarthick44 <jeyakarthick408@gmail.com>
2022-03-17 23:44:46 +0530

    first commit

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 5000;
 const cors = require("cors");
 app.use(cors());
+app.get("/", function (req, res) {
+  res.send("");
+});
 
 let jsonFile = require("./data.json");
 
